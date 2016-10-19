4bd0febc08796eddd79502aa6340bb3d6aa53903
Conny Karlsson <Conny Karlsson>
2016-10-19 22:19:59 +0200

    BUG - Added namespaces to autoloader
    - Added psr4prefix for core event http and framework

--- init/autoloader.php
+++ init/autoloader.php
@@ -2,6 +2,11 @@
 require VENROOT . 'gungnir-mvc/core/src/Autoloader.php';
 $autoloader = new \Gungnir\Core\Autoloader(ROOT);
 
+$autoloader->psr4prefix('Gungnir\Core', VENROOT . 'gungnir-mvc/core/src');
+$autoloader->psr4prefix('Gungnir\HTTP', VENROOT . 'gungnir-mvc/http/src');
+$autoloader->psr4prefix('Gungnir\Event', VENROOT . 'gungnir-mvc/event/src');
+$autoloader->psr4prefix('Gungnir\Framework', VENROOT . 'gungnir-mvc/framework/src');
+
 if (file_exists(APPROOT.'init/autoloader.php')) {
     $prefixes = require APPROOT.'init/autoloader.php';
     foreach ($prefixes as $prefix => $src) {
